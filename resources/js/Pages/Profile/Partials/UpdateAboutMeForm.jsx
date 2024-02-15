5996a4093995dab2af93d5b0f96617a7eb07e09b
kpwhelan <60331971+kpwhelan@users.noreply.github.com>
2024-02-15 18:19:39 -0500

    update about me form to use axios instead of inertia

--- resources/js/Pages/Profile/Partials/UpdateAboutMeForm.jsx
+++ resources/js/Pages/Profile/Partials/UpdateAboutMeForm.jsx
@@ -4,9 +4,12 @@ import TextInput from "@/Components/TextInput";
 import { useForm, usePage } from "@inertiajs/react";
 import PrimaryButton from "@/Components/PrimaryButton";
 import { Transition } from "@headlessui/react";
+import { useState } from "react";
+import axios from "axios";
 
 export default function UpdateAboutMeForm({ className = '' }) {
     const user = usePage().props.auth.user;
+    const [isProcessing, setIsProcessing] = useState(false);
 
     const { data, setData, patch, errors, processing, recentlySuccessful } = useForm({
         about_me: user.about_me,
@@ -15,7 +18,29 @@ export default function UpdateAboutMeForm({ className = '' }) {
     const submit = (e) => {
         e.preventDefault();
 
-        patch(route('profile.update'));
+        //if no changes were made, don't submit
+        if (!isDirty) return;
+
+        setIsProcessing(true);
+
+        axios.patch(route('profile.edit'), {
+            about_me: data.about_me
+        })
+        .then(res => {
+            setIsProcessing(false);
+            notifySuccess(res.data.data.message);
+        })
+        .catch(error => {
+            setIsProcessing(false);
+
+            if (error.response.data.errors) {
+                for (const [key, value] of Object.entries(error.response.data.errors)) {
+                    errors[key] = value;
+                }
+            }
+
+            notifyError(error.response.data.message);
+        })
     }
 
     return (
@@ -35,6 +60,7 @@ export default function UpdateAboutMeForm({ className = '' }) {
                         value={data.about_me}
                         className="block h-72 overflow-scroll p-2.5 w-full text-md text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-gray-500 focus:border-gray-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500"
                         autoComplete="about_me"
+                        disabled={isProcessing}
 
                         onChange={(e) => setData('about_me', e.target.value)}
                     />
@@ -43,7 +69,7 @@ export default function UpdateAboutMeForm({ className = '' }) {
                 </div>
 
                 <div className="flex items-center gap-4">
-                    <PrimaryButton disabled={processing}>Save</PrimaryButton>
+                    <PrimaryButton disabled={isProcessing}>Save</PrimaryButton>
 
                     <Transition
                         show={recentlySuccessful}
