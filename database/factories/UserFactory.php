1d572930401aa7f06c1c8b13825d6445db052526
Kevin Whelan <kevinwhelandev@gmail.com>
2024-05-03 13:54:45 -0700

    add username to User factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,7 @@ class UserFactory extends Factory
             'title' => fake()->jobTitle(),
             'about_me' => fake()->paragraph(),
             'email' => fake()->unique()->safeEmail(),
+            'username' => fake()->userName(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
